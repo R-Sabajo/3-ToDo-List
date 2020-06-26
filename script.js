02c0200830b4115819d1290012e33dce81dae0e2
Rama Sabajo <rsabajo@gmail.com>
2020-06-26 13:40:41 +0200

    refactor code

--- script.js
+++ script.js
@@ -1,35 +1,44 @@
-const taskList = document.getElementById('taskList');
+// select html form elements from DOM
 const addField = document.getElementById('newTaskField');
 const addButton = document.getElementById('newTaskAdd');
+const taskList = document.getElementById('taskList');
 
+// function to create html listitems for each task
 const addTaskToDOM = task => {
-  let newLi = document.createElement('li');
+  const newLi = document.createElement('li');
+
   let newCheckbox = document.createElement('input');
   newCheckbox.setAttribute('type', 'checkbox');
-  newCheckbox.setAttribute(`value`, `${task.id}`);
+  newCheckbox.setAttribute('value', task.id);
+
   let newTaskDescription = document.createElement('p');
   newTaskDescription.innerHTML = `${task.description}`;
-  console.log(newTaskDescription);
+
   if (task.done === 'true') {
     newCheckbox.setAttribute('checked', 'checked');
     newTaskDescription.innerHTML = `<del>${task.description}</del>`;
   } else {
     newTaskDescription.innerHTML = `${task.description}`;
   }
+
+  const newImg = document.createElement('img');
+
+  newImg.src = `./trash-delete-icon.jpg`;
+  newImg.value = task.id;
+  newImg.width = `20`;
+
   newLi.appendChild(newCheckbox);
   newLi.appendChild(newTaskDescription);
-  taskList.appendChild(newLi);
-  let newImg = document.createElement('img');
   newLi.appendChild(newImg);
-  newImg.src = `./trash-delete-icon.jpg`;
-  newImg.value = `${task.id}`;
-  newImg.width = `20`;
-  newImg.addEventListener('click', event => {
-    deleteTask(event.target.value);
-    taskList.removeChild(newLi);
-  });
+  taskList.appendChild(newLi);
 
-  newCheckbox.addEventListener('change', event => {
+  checkBtn(newCheckbox, task);
+  delBtn(newImg, newLi);
+};
+
+// function to add eventlistener to change set the task.done value
+const checkBtn = (btn, task) =>
+  btn.addEventListener('change', event => {
     if (task.done === 'true') {
       putTask(event.target.value, `${task.description}`, 'false').then(() =>
         allTaskItems(getTasks())
@@ -40,8 +49,15 @@ const addTaskToDOM = task => {
       );
     }
   });
-};
 
+// function to add eventlistener to trash icon to delete a task
+const delBtn = (btn, li) =>
+  btn.addEventListener('click', event => {
+    deleteTask(event.target.value);
+    taskList.removeChild(li);
+  });
+
+// function to get all taskitems
 const allTaskItems = async anyTask => {
   taskList.innerHTML = '';
   const data = await anyTask;
@@ -52,6 +68,7 @@ const allTaskItems = async anyTask => {
 
 allTaskItems(getTasks());
 
+// function to be able to press enter to add a task
 addField.addEventListener('keydown', async event => {
   if (event.keyCode === 13) {
     postTask(`${event.target.value}`, 'false').then(() =>
@@ -61,6 +78,7 @@ addField.addEventListener('keydown', async event => {
   }
 });
 
+// function to add a task via the Add button
 addButton.addEventListener('click', async event => {
   postTask(`${addField.value}`, 'false').then(() => allTaskItems(getTasks()));
   addField.value = '';
